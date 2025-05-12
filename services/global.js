de1635f1158c07340b7d130f02a819576bba7471
yuvalsigal16 <yuvalsigal16@gmail.com>
2025-05-12 11:47:04 +0300

    הוספת חושך אור

--- services/global.js
+++ services/global.js
@@ -1,3 +1,15 @@
 
     document.getElementById("hamburger").addEventListener("click", function()
     {document.querySelector(".navbar").classList.toggle("open"); });
+
+    
+   const themeToggle = document.querySelector(".theme-toggle");
+   const body = document.body;
+
+   themeToggle.addEventListener("click", () => {
+      body.classList.toggle("dark-mode");
+
+      if (body.classList.contains("dark-mode")) {
+         localStorage.setItem("theme", "dark");} 
+    else {
+         localStorage.setItem("theme", "light"); }});
