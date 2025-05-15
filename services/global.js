eb1b9737497ceb2959192f90bd26617933499803
yuvalsigal16 <yuvalsigal16@gmail.com>
2025-05-15 16:31:51 +0300

    טיפלתי בעיצובים, סידורים למובייל, תיקונים אחרונים

--- services/global.js
+++ services/global.js
@@ -1,19 +1,22 @@
 
 document.querySelector("#hamburger").onclick = () => 
-   document.querySelector(".navbar").classList.toggle("open");
+document.querySelector(".navbar").classList.toggle("open");
 
-    
-   const themeToggle = document.querySelector(".theme-toggle");
-   const body = document.body;
+const themeIcons = document.querySelectorAll(".theme-toggle img");
+const body = document.body;
 
-   themeToggle.onclick = function() {
-      body.classList.toggle("dark-mode");
-   
-      if (body.classList.contains("dark-mode")) {
-         localStorage.setItem("theme", "dark");
-      } else {
-         localStorage.setItem("theme", "light");
-      }
-   };
+if (localStorage.getItem("theme") === "dark") {
+  body.classList.add("dark-mode");
+}
 
- 
+themeIcons.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    body.classList.toggle("dark-mode");
+
+    if (body.classList.contains("dark-mode")) {
+      localStorage.setItem("theme", "dark");
+    } else {
+      localStorage.setItem("theme", "light");
+    }
+  });
+});
